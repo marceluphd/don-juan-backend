44924a17e42e473994e319dcd5c7ecda0e697df6
Luiz Claudio <luizcns18@gmail.com>
2019-06-29 02:07:12 -0300

    feat: Seed types

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -16,6 +16,8 @@ const Role = use('Adonis/Acl/Role')
 
 const Product = use('App/Models/Product')
 
+const Type = use('App/Models/Type')
+
 class DatabaseSeeder {
   async run () {
     // Users
@@ -49,39 +51,112 @@ class DatabaseSeeder {
     await regularUser.roles().attach([userRole.id])
 
     // Products
-    await Product.create({
+    const pizzaProduct = await Product.create({
       'name': 'Pizzas',
       'description': 'More than 50 pizza flavors at 4 different sizes',
       'image': 'https://s3.amazonaws.com/bootcamp.fs/pizzas@1x.png',
       'time': 30
     })
 
-    await Product.create({
+    const pastaProduct = await Product.create({
       'name': 'Pasta',
       'description': '10 kinds of pasta with different sauces to satisfy you',
       'image': 'https://s3.amazonaws.com/bootcamp.fs/massas@1x.png',
       'time': 25
     })
 
-    await Product.create({
+    const calzoneProduct = await Product.create({
       'name': 'Calzones',
       'description': 'Stuffed calzones with over than 50 flavors',
       'image': 'https://s3.amazonaws.com/bootcamp.fs/calzones@1x.png',
       'time': 15
     })
 
-    await Product.create({
-      'name': 'Non-alcoholic drinks',
-      'description': 'Soda, juice, iced tea, energy driks and water',
-      'image': 'https://s3.amazonaws.com/bootcamp.fs/bebidas@1x.png',
-      'time': 5
+    // const nonAlcoholicProduct = await Product.create({
+    //   'name': 'Non-alcoholic drinks',
+    //   'description': 'Soda, juice, iced tea, energy driks and water',
+    //   'image': 'https://s3.amazonaws.com/bootcamp.fs/bebidas@1x.png',
+    //   'time': 5
+    // })
+
+    // const alcoholicProduct = await Product.create({
+    //   'name': 'Alcoholic drinks',
+    //   'description': 'Craft beer, wine and distillate drink',
+    //   'image': 'https://s3.amazonaws.com/bootcamp.fs/bebidas-alcoolicas@1x.png',
+    //   'time': 5
+    // })
+
+    // Types
+    const pepperoniType = await Type.create({
+      'name': 'Pepperoni',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/1.png',
+      'product_id': pizzaProduct.id
     })
 
-    await Product.create({
-      'name': 'Alcoholic drinks',
-      'description': 'Craft beer, wine and distillate drink',
-      'image': 'https://s3.amazonaws.com/bootcamp.fs/bebidas-alcoolicas@1x.png',
-      'time': 5
+    const margueriteType = await Type.create({
+      'name': 'Marguerite',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/2.png',
+      'product_id': pizzaProduct.id
+    })
+
+    const portugueseType = await Type.create({
+      'name': 'Portuguese',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/3.png',
+      'product_id': pizzaProduct.id
+    })
+
+    const chickenType = await Type.create({
+      'name': 'Chicken',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/4.png',
+      'product_id': pizzaProduct.id
+    })
+
+    const fettucineType = await Type.create({
+      'name': 'Fettucine',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/5.png',
+      'product_id': pastaProduct.id
+    })
+
+    const spaghettiType = await Type.create({
+      'name': 'Spaghetti',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/6.png',
+      'product_id': pastaProduct.id
+    })
+
+    const macaroniType = await Type.create({
+      'name': 'Macaroni',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/1.png',
+      'product_id': pastaProduct.id
+    })
+
+    const ravioliType = await Type.create({
+      'name': 'Ravioli',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/2.png',
+      'product_id': pastaProduct.id
+    })
+
+    const classicType = await Type.create({
+      'name': 'Classic',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/3.png',
+      'product_id': calzoneProduct.id
+    })
+
+    const spinachType = await Type.create({
+      'name': 'Spinach',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/4.png',
+      'product_id': calzoneProduct.id
+    })
+
+    const ricottaType = await Type.create({
+      'name': 'Ricotta',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/5.png',
+      'product_id': calzoneProduct.id
+    })
+
+    const sausageType = await Type.create({
+      'name': 'Sausage',
+      'image': 'https://s3.amazonaws.com/bootcamp.fs/Pizzas/6.png',
+      'product_id': calzoneProduct.id
     })
   }
 }
